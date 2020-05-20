8b1a94a85c023c72c224e1b181430add5ebf328c
wushilaowang@163.com <wushilaowang@163.com>
2020-05-20 18:57:41 +0800

    5-20

--- src/main/java/com/oupu/gradefour/controller/WordController.java
+++ src/main/java/com/oupu/gradefour/controller/WordController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
 import java.util.List;
 
 @RestController
@@ -15,6 +16,26 @@ public class WordController {
 
     @RequestMapping("/getWord")
     public List<Word> getWord() {
+        File file = new File("C:\\Users\\wushi\\Documents\\Tencent Files\\3072388337\\FileRecv\\MobileFile");
+        System.out.println(file.exists() && file.exists());
+        if(file.isFile() && file.exists()) {
+            try {
+                InputStreamReader reader = new InputStreamReader(
+                        new FileInputStream(file)
+                );
+                BufferedReader bf = new BufferedReader(reader);
+                String lineTxt = null;
+                int a = 1;
+                while((lineTxt = bf.readLine()) != null && a < 1) {
+                    System.out.println(lineTxt);
+                    a--;
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return wordService.getWord();
     }
 }
